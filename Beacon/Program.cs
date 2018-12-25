722450d79c1f73aaf1e129259240488b5a3fd329
thiseas.matth <thiseas.matth@outlook.com>
2018-12-25 23:56:12 +0200

    You can now register as new user

--- Beacon/Program.cs
+++ Beacon/Program.cs
@@ -23,7 +23,61 @@ namespace Beacon
             Console.WriteLine("X - Terminate");
             Console.WriteLine("==============");
 
-            Console.ReadLine();
+            string Selection = Console.ReadLine();
+
+            switch (Selection)
+            {
+                case "X":
+                case "x":
+                    Console.WriteLine("Thank you for choosing us for your communication needs.");
+                    Console.WriteLine("The program will now terminate.");
+                    Environment.Exit(0);
+                    break;
+                case "R":
+                case "r":
+
+                    Console.WriteLine("Please pick a username:");
+                    string Name = Console.ReadLine();
+                    //TODO check for username duplication
+
+                    string Pass1;
+                    string Pass2;
+
+                    do
+                    {
+                        Console.WriteLine("Please pick a password:");
+                        Pass1 = Console.ReadLine();
+
+                        Console.WriteLine("Please enter your password a second time:");
+                        Pass2 = Console.ReadLine();
+
+                        if (Pass1 != Pass2)
+                        {
+                            Console.WriteLine("The passwords do not match!");
+                        }
+
+                    } while (Pass1 != Pass2);
+
+                    Guest guest = new Guest(Name, Pass1, Authorization.Guest);
+
+                    //TODO add BASE interaction
+
+                    Console.WriteLine($"The user {Name} has been created.");
+                    Console.WriteLine("Please reopen the application to login with your credentials.");                    
+                    Console.WriteLine("Press ANY key to terminate:");
+                    Console.ReadKey();
+                    Environment.Exit(0);
+                    break;
+
+
+
+
+
+
+
+
+            }
+            
         }
     }
 }
